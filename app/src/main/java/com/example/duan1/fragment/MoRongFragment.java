1652f32422fc46319fb875e2b7d832239721a558
Admin <Nguyenthanhnoem2k27@gmail.com>
2019-12-01 14:46:55 +0700

    MVP & DataBinding

--- app/src/main/java/com/example/duan1/fragment/MoRongFragment.java
+++ app/src/main/java/com/example/duan1/fragment/MoRongFragment.java
@@ -12,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.cardview.widget.CardView;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import com.example.duan1.R;
@@ -20,6 +21,7 @@ import com.example.duan1.activity.BoChuyenDoiActivity;
 import com.example.duan1.activity.CaiDatActivity;
 import com.example.duan1.activity.ThuVienActivity;
 import com.example.duan1.activity.ThucPhamActivity;
+import com.example.duan1.databinding.FragmentMoRongBinding;
 import com.example.duan1.inteface.MoRong_Interface;
 import com.example.duan1.presenter.MoRong_Precenter;
 
@@ -27,66 +29,19 @@ public class MoRongFragment extends Fragment implements MoRong_Interface {
 
     private MoRong_Precenter moRong_precenter;
 
+    //khi chinh xong layout thi se co ham nay
+    FragmentMoRongBinding binding;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_mo_rong, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_mo_rong, container, false);
         moRong_precenter = new MoRong_Precenter(this);
-        CardView cvLibrary = view.findViewById(R.id.cvLibrary);
-        CardView cvConvert = view.findViewById(R.id.cvConvert);
-        CardView cvSetting = view.findViewById(R.id.cvSetting);
-        CardView cvFood_MR = view.findViewById(R.id.cvFood_MR);
-        CardView cvExercise_MR = view.findViewById(R.id.cvExercise_MR);
-        CardView cvReset = view.findViewById(R.id.cvReset);
 
+        //khai bao precenter trong xml
+        binding.setMorongprecenter(moRong_precenter);
 
-        cvLibrary.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                moRong_precenter.setJob_cvLibrary();
-            }
-        });
-
-        cvConvert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                moRong_precenter.setJob_cvConvert();
-            }
-        });
-
-        cvSetting.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                moRong_precenter.setJob_cvSetting();
-            }
-        });
-
-        cvFood_MR.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                moRong_precenter.setJob_cvFood_MR();
-            }
-        });
-
-
-        cvExercise_MR.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                moRong_precenter.setJob_cvExercise_MR();
-            }
-        });
-
-
-        cvReset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                moRong_precenter.setJob_cvReset();
-            }
-        });
-        return view;
+        return binding.getRoot();
     }
 
     @Override
